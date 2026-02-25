53b5f9c69a0fc467eba32a943adb34da0a989eaf
its-ryann <kikayiryankim@gmail.com>
2026-02-25 16:02:01 +0300

    Implemented the conflict resolution logic

--- internal/services/conflict_service.go
+++ internal/services/conflict_service.go
@@ -8,9 +8,27 @@ func NewConflictService() *ConflictService { // Creates a new ConflictService in
 }
 
 func (c *ConflictService) ResolveProduceConflict(local, server Produce) Produce { // Decides which Produce to keep based on version/timestamp
-	return server
+	if local.Version > server.Version {
+		return local
+	} else if server.Version > local.Version {
+		return server
+	} else {
+		if local.UpdatedAt.After(server.UpdatedAt) { // If versions are equal, use timestamp to decide
+			return local
+		}
+		return server
+	}
 }
 
 func (c *ConflictService) ResolveListingConflict(local, server Listing) Listing { // Decides which Listing to keep based on version/timestamp
-	return server
+	if local.Version > server.Version {
+		return local
+	} else if server.Version > local.Version {
+		return server
+	} else {
+		if local.UpdatedAt.After(server.UpdatedAt) { // If versions are equal, use timestamp to decide
+			return local
+		}
+		return server
+	}
 }
